e738b97f8abed991420524a9594909513f74ba3c
TheDr0l <dilanosorio0717@gmail.com>
2025-04-10 20:48:19 -0500

    Proyecto relativamente listo

--- src/main/java/com/example/IceCream_SpringBoot/controller/AuthController.java
+++ src/main/java/com/example/IceCream_SpringBoot/controller/AuthController.java
@@ -2,7 +2,6 @@ package com.example.IceCream_SpringBoot.controller;
 
 import com.example.IceCream_SpringBoot.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
